b9bfa2a34c50620548213f055b575435104657d5
Patricia Diego Palazuelos <34513615+pdp79@users.noreply.github.com>
2025-04-16 09:52:25 +0200

    optimized summation in __total__

--- cobi/mle/ls.py
+++ cobi/mle/ls.py
@@ -93,6 +93,26 @@ class LinearSystem:
                 self.Q      = np.zeros(self.ext_dim, dtype=self.dt)
                 self.V      = np.zeros(self.ext_dim, dtype=self.dt)
 
+
+    def summation(self, opt, v_ij, v_pq):
+        x_ijpq = np.zeros((self.Nbands, self.Nbands, self.Nbands, self.Nbands), dtype=self.dt)
+        ii     = self.mle.MNi.ravel()
+        jj     = self.mle.MNj.ravel()
+        pp     = self.mle.MNp.ravel()
+        qq     = self.mle.MNq.ravel()
+        v1     = v_ij[self.mle.MNi, self.mle.MNj, :]
+        v2     = v_pq[self.mle.MNp, self.mle.MNq, :]
+        x_ijpq[ii, jj, pp, qq] = np.sum(v1 * self.iC * v2, axis=2).ravel()
+        if opt == 'ijpq':
+            return x_ijpq
+        elif opt == 'ij':
+            return np.sum(np.sum(x_ijpq, axis=3), axis=2)
+        elif opt == '_':
+            return np.sum(x_ijpq)
+        else:
+            raise ValueError(f'{opt} is not a valid option')
+       
+            
     def compute_terms(self):
         if self.mode=="total":
             return self.__total__()
@@ -100,7 +120,8 @@ class LinearSystem:
             return self.__cumulative__() 
         elif self.mode=="ell":
             return self.__ell__()
-          
+     
+    # not optimized
     def __cumulative__(self):
         for MN_pair in self.mle.MNidx:
             ii, jj, pp, qq, mm, nn = self.mle.get_index(MN_pair)
@@ -172,79 +193,77 @@ class LinearSystem:
                     self.Q[:]            += np.cumsum(self.bin_cl['EBs_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EsBd_ij_b'][pp,qq,:])
                     self.V[:]            += np.cumsum(self.bin_cl['EBs_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EdBs_ij_b'][pp,qq,:])
    
-    #TODO PDP: these ones can be further optimised but I'm leaving them like this
-    # for now to debug the new code structure first
+    # optimised
     def __total__(self):
-        for MN_pair in self.mle.MNidx:
-            ii, jj, pp, qq, mm, nn = self.mle.get_index(MN_pair)
-            # common to all fits (basic alpha*alpha fit)
-            self.B_ijpq[ii,jj,pp,qq]    = np.sum(self.bin_cl['BBo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['BBo_ij_b'][pp,qq,:])
-            self.E_ijpq[ii,jj,pp,qq]    = np.sum(self.bin_cl['EEo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EEo_ij_b'][pp,qq,:])
-            self.I_ijpq[ii,jj,pp,qq]    = np.sum(self.bin_cl['BBo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EEo_ij_b'][pp,qq,:]) 
-            self.D_ij[ii,jj]           += np.sum(self.bin_cl['EEo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EBo_ij_b'][pp,qq,:])
-            self.H_ij[ii,jj]           += np.sum(self.bin_cl['BBo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EBo_ij_b'][pp,qq,:])
-            self.A                     += np.sum(self.bin_cl['EBo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EBo_ij_b'][pp,qq,:])
-            # only used in particular combinations 
+        # common to all fits (basic alpha*alpha fit)
+        self.B_ijpq         = self.summation('ijpq', self.bin_cl['BBo_ij_b'],   self.bin_cl['BBo_ij_b'])
+        self.E_ijpq         = self.summation('ijpq', self.bin_cl['EEo_ij_b'],   self.bin_cl['EEo_ij_b'])
+        self.I_ijpq         = self.summation('ijpq', self.bin_cl['BBo_ij_b'],   self.bin_cl['EEo_ij_b']) 
+        self.D_ij           = self.summation('ij',   self.bin_cl['EEo_ij_b'],   self.bin_cl['EBo_ij_b'])
+        self.H_ij           = self.summation('ij',   self.bin_cl['BBo_ij_b'],   self.bin_cl['EBo_ij_b'])
+        self.A              = self.summation('_',    self.bin_cl['EBo_ij_b'],   self.bin_cl['EBo_ij_b'])
+        # only used in particular combinations 
+        if 'beta' in self.fit:
+            # beta*beta and beta*alpha 
+            self.tau_ij     = self.summation('ij',   self.bin_cl['EEo_ij_b'],   self.bin_cl['EEcmb_ij_b'])
+            self.varphi_ij  = self.summation('ij',   self.bin_cl['EEo_ij_b'],   self.bin_cl['BBcmb_ij_b'])
+            self.ene_ij     = self.summation('ij',   self.bin_cl['BBo_ij_b'],   self.bin_cl['EEcmb_ij_b'])
+            self.epsilon_ij = self.summation('ij',   self.bin_cl['BBo_ij_b'],   self.bin_cl['BBcmb_ij_b'])
+            self.C          = self.summation('_',    self.bin_cl['EEcmb_ij_b'], self.bin_cl['BBcmb_ij_b'])
+            self.F          = self.summation('_',    self.bin_cl['EEcmb_ij_b'], self.bin_cl['EEcmb_ij_b']) 
+            self.G          = self.summation('_',    self.bin_cl['BBcmb_ij_b'], self.bin_cl['BBcmb_ij_b']) 
+            self.O          = self.summation('_',    self.bin_cl['EBo_ij_b'],   self.bin_cl['EEcmb_ij_b']) 
+            self.P          = self.summation('_',    self.bin_cl['EBo_ij_b'],   self.bin_cl['BBcmb_ij_b'])
+        if 'Ad' in self.fit:
+            # Ad*Ad and Ad*alpha
+            self.sigma_ij   = self.summation('ij',   self.bin_cl['EEo_ij_b'],   self.bin_cl['EBd_ij_b'])
+            self.omega_ij   = self.summation('ij',   self.bin_cl['BBo_ij_b'],   self.bin_cl['EBd_ij_b'])
+            self.R          = self.summation('_',    self.bin_cl['EBd_ij_b'],   self.bin_cl['EBd_ij_b'])
+            self.N          = self.summation('_',    self.bin_cl['EBo_ij_b'],   self.bin_cl['EBd_ij_b'])
             if 'beta' in self.fit:
-                # beta*beta and beta*alpha 
-                self.tau_ij[ii,jj]     += np.sum(self.bin_cl['EEo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EEcmb_ij_b'][pp,qq,:])
-                self.varphi_ij[ii,jj]  += np.sum(self.bin_cl['EEo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['BBcmb_ij_b'][pp,qq,:])
-                self.ene_ij[ii,jj]     += np.sum(self.bin_cl['BBo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EEcmb_ij_b'][pp,qq,:])
-                self.epsilon_ij[ii,jj] += np.sum(self.bin_cl['BBo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['BBcmb_ij_b'][pp,qq,:])
-                self.C                 += np.sum(self.bin_cl['EEcmb_ij_b'][ii,jj,:] *self.iC[:,mm,nn]* self.bin_cl['BBcmb_ij_b'][pp,qq,:]) 
-                self.F                 += np.sum(self.bin_cl['EEcmb_ij_b'][ii,jj,:] *self.iC[:,mm,nn]* self.bin_cl['EEcmb_ij_b'][pp,qq,:]) 
-                self.G                 += np.sum(self.bin_cl['BBcmb_ij_b'][ii,jj,:] *self.iC[:,mm,nn]* self.bin_cl['BBcmb_ij_b'][pp,qq,:]) 
-                self.O                 += np.sum(self.bin_cl['EBo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EEcmb_ij_b'][pp,qq,:]) 
-                self.P                 += np.sum(self.bin_cl['EBo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['BBcmb_ij_b'][pp,qq,:])
+                # Ad*beta
+                self.LAMBDA = self.summation('_',    self.bin_cl['EBd_ij_b'],   self.bin_cl['EEcmb_ij_b'])
+                self.mu     = self.summation('_',    self.bin_cl['EBd_ij_b'],   self.bin_cl['BBcmb_ij_b'])
+        if 'As' in self.fit:
+            # As*As and As*alpha
+            self.nu_ij      = self.summation('ij',   self.bin_cl['EEo_ij_b'],   self.bin_cl['EBs_ij_b'])
+            self.psi_ij     = self.summation('ij',   self.bin_cl['BBo_ij_b'],   self.bin_cl['EBs_ij_b']) 
+            self.S          = self.summation('_',    self.bin_cl['EBs_ij_b'],   self.bin_cl['EBs_ij_b'])
+            self.J          = self.summation('_',    self.bin_cl['EBo_ij_b'],   self.bin_cl['EBs_ij_b'])
+            if 'beta' in self.fit:
+                # As*beta
+                self.X      = self.summation('_',    self.bin_cl['EBs_ij_b'],   self.bin_cl['EEcmb_ij_b'])
+                self.Y      = self.summation('_',    self.bin_cl['EBs_ij_b'],   self.bin_cl['BBcmb_ij_b'])
             if 'Ad' in self.fit:
-                # Ad*Ad and Ad*alpha
-                self.sigma_ij[ii,jj]   += np.sum(self.bin_cl['EEo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EBd_ij_b'][pp,qq,:])
-                self.omega_ij[ii,jj]   += np.sum(self.bin_cl['BBo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EBd_ij_b'][pp,qq,:])
-                self.R                 += np.sum(self.bin_cl['EBd_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EBd_ij_b'][pp,qq,:])
-                self.N                 += np.sum(self.bin_cl['EBo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EBd_ij_b'][pp,qq,:])
-                if 'beta' in self.fit:
-                    # Ad*beta
-                    self.LAMBDA        += np.sum(self.bin_cl['EBd_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EEcmb_ij_b'][pp,qq,:])
-                    self.mu            += np.sum(self.bin_cl['EBd_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['BBcmb_ij_b'][pp,qq,:])
+                # As*Ad
+                self.W      = self.summation('_',    self.bin_cl['EBs_ij_b'],   self.bin_cl['EBd_ij_b'])
+        if 'Asd' in self.fit:
+            # Asd*Asd and Asd*alpha
+            self.pi_ij      = self.summation('ij',   self.bin_cl['EEo_ij_b'],   self.bin_cl['EsBd_ij_b'])
+            self.rho_ij     = self.summation('ij',   self.bin_cl['EEo_ij_b'],   self.bin_cl['EdBs_ij_b'])
+            self.phi_ij     = self.summation('ij',   self.bin_cl['BBo_ij_b'],   self.bin_cl['EsBd_ij_b'])
+            self.OMEGA_ij   = self.summation('ij',   self.bin_cl['BBo_ij_b'],   self.bin_cl['EdBs_ij_b'])
+            self.T          = self.summation('_',    self.bin_cl['EsBd_ij_b'],  self.bin_cl['EsBd_ij_b'])
+            self.U          = self.summation('_',    self.bin_cl['EdBs_ij_b'],  self.bin_cl['EdBs_ij_b'])
+            self.Z          = self.summation('_',    self.bin_cl['EsBd_ij_b'],  self.bin_cl['EdBs_ij_b'])
+            self.M          = self.summation('_',    self.bin_cl['EBo_ij_b'],   self.bin_cl['EdBs_ij_b'])
+            self.L          = self.summation('_',    self.bin_cl['EBo_ij_b'],   self.bin_cl['EsBd_ij_b'])
+            if 'beta' in self.fit:
+                # Asd*beta
+                self.DELTA  = self.summation('_',    self.bin_cl['EsBd_ij_b'],  self.bin_cl['EEcmb_ij_b']) 
+                self.eta    = self.summation('_',    self.bin_cl['EdBs_ij_b'],  self.bin_cl['EEcmb_ij_b']) 
+                self.theta  = self.summation('_',    self.bin_cl['EdBs_ij_b'],  self.bin_cl['BBcmb_ij_b']) 
+                self.delta  = self.summation('_',    self.bin_cl['EsBd_ij_b'],  self.bin_cl['BBcmb_ij_b'])
+            if 'Ad' in self.fit:
+                # Asd*Ad
+                self.K      = self.summation('_',    self.bin_cl['EdBs_ij_b'],  self.bin_cl['EBd_ij_b'])
+                self.xi     = self.summation('_',    self.bin_cl['EsBd_ij_b'],  self.bin_cl['EBd_ij_b'])
             if 'As' in self.fit:
-                # As*As and As*alpha
-                self.nu_ij[ii,jj]      += np.sum(self.bin_cl['EEo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EBs_ij_b'][pp,qq,:])
-                self.psi_ij[ii,jj]     += np.sum(self.bin_cl['BBo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EBs_ij_b'][pp,qq,:]) 
-                self.S                 += np.sum(self.bin_cl['EBs_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EBs_ij_b'][pp,qq,:])
-                self.J                 += np.sum(self.bin_cl['EBo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EBs_ij_b'][pp,qq,:])
-                if 'beta' in self.fit:
-                    # As*beta
-                    self.X             += np.sum(self.bin_cl['EBs_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EEcmb_ij_b'][pp,qq,:])
-                    self.Y             += np.sum(self.bin_cl['EBs_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['BBcmb_ij_b'][pp,qq,:])
-                if 'Ad' in self.fit:
-                    # As*Ad
-                    self.W             += np.sum(self.bin_cl['EBs_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EBd_ij_b'][pp,qq,:])
-            if 'Asd' in self.fit:
-                # Asd*Asd and Asd*alpha
-                self.pi_ij[ii,jj]      += np.sum(self.bin_cl['EEo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EsBd_ij_b'][pp,qq,:])
-                self.rho_ij[ii,jj]     += np.sum(self.bin_cl['EEo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EdBs_ij_b'][pp,qq,:])
-                self.phi_ij[ii,jj]     += np.sum(self.bin_cl['BBo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EsBd_ij_b'][pp,qq,:])
-                self.OMEGA_ij[ii,jj]   += np.sum(self.bin_cl['BBo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EdBs_ij_b'][pp,qq,:])
-                self.T                 += np.sum(self.bin_cl['EsBd_ij_b'][ii,jj,:]  *self.iC[:,mm,nn]* self.bin_cl['EsBd_ij_b'][pp,qq,:])
-                self.U                 += np.sum(self.bin_cl['EdBs_ij_b'][ii,jj,:]  *self.iC[:,mm,nn]* self.bin_cl['EdBs_ij_b'][pp,qq,:])
-                self.Z                 += np.sum(self.bin_cl['EsBd_ij_b'][ii,jj,:]  *self.iC[:,mm,nn]* self.bin_cl['EdBs_ij_b'][pp,qq,:])
-                self.M                 += np.sum(self.bin_cl['EBo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EdBs_ij_b'][pp,qq,:])
-                self.L                 += np.sum(self.bin_cl['EBo_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EsBd_ij_b'][pp,qq,:])
-                if 'beta' in self.fit:
-                    # Asd*beta
-                    self.DELTA         += np.sum(self.bin_cl['EsBd_ij_b'][ii,jj,:]  *self.iC[:,mm,nn]* self.bin_cl['EEcmb_ij_b'][pp,qq,:]) 
-                    self.eta           += np.sum(self.bin_cl['EdBs_ij_b'][ii,jj,:]  *self.iC[:,mm,nn]* self.bin_cl['EEcmb_ij_b'][pp,qq,:]) 
-                    self.theta         += np.sum(self.bin_cl['EdBs_ij_b'][ii,jj,:]  *self.iC[:,mm,nn]* self.bin_cl['BBcmb_ij_b'][pp,qq,:]) 
-                    self.delta         += np.sum(self.bin_cl['EsBd_ij_b'][ii,jj,:]  *self.iC[:,mm,nn]* self.bin_cl['BBcmb_ij_b'][pp,qq,:])
-                if 'Ad' in self.fit:
-                    # Asd*Ad
-                    self.K             += np.sum(self.bin_cl['EdBs_ij_b'][ii,jj,:]  *self.iC[:,mm,nn]* self.bin_cl['EBd_ij_b'][pp,qq,:])
-                    self.xi            += np.sum(self.bin_cl['EsBd_ij_b'][ii,jj,:]  *self.iC[:,mm,nn]* self.bin_cl['EBd_ij_b'][pp,qq,:])
-                if 'As' in self.fit:
-                    # Asd*As
-                    self.Q             += np.sum(self.bin_cl['EBs_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EsBd_ij_b'][pp,qq,:])
-                    self.V             += np.sum(self.bin_cl['EBs_ij_b'][ii,jj,:]   *self.iC[:,mm,nn]* self.bin_cl['EdBs_ij_b'][pp,qq,:])
+                # Asd*As
+                self.Q      = self.summation('_',    self.bin_cl['EBs_ij_b'],   self.bin_cl['EsBd_ij_b'])
+                self.V      = self.summation('_',    self.bin_cl['EBs_ij_b'],   self.bin_cl['EdBs_ij_b'])
 
+    # not optimized
     def __ell__(self):
         for MN_pair in self.mle.MNidx:
             ii, jj, pp, qq, mm, nn = self.mle.get_index(MN_pair)
