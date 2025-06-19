6c25410bf5d8d48742ab7486e85bd5eddc0beedb
Carlos Hervias <aconcagua.chc@gmail.com>
2025-06-19 22:17:20 +0000

    changing the run script

--- jobs/run.py
+++ jobs/run.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sys
 import argparse
-sys.path.append('/global/homes/l/lonappan/workspace/cobi')
+sys.path.append('/home/chervias/CMBwork/Cosmic_birefringence/cobi')
 from cobi import mpi
 from cobi.simulation import LATsky
 from cobi.spectra import Spectra
@@ -18,47 +18,26 @@ parser.add_argument('-mle', action='store_true', help='Run the MLE')
 args = parser.parse_args()
 
 # Constants
-libdir = '/global/cfs/cdirs/sobs/cosmic_birefringence/v0'
+libdir = '/home/chervias/CMBwork/Cosmic_birefringence/output_cobi/my_test_ds'
 nside = 2048
 cb_model = "iso"
 beta = 0.35
+mask = '/home/chervias/CMBwork/SimonsObs/Masks/Mask_fullLAT_5.0_deg_gal_with-srcs-galplane80.fits'
+nhits = '/home/chervias/CMBwork/SimonsObs/data/LAT_ivar_galcoords.fits'
+nhits_fac = 1.052
 
 #setting 1
-# alpha = 0
-# alpha_err = 0
-# bp = False
-# nm = 'NC'
-
-#setting 2
-# alpha = 0
-# alpha_err = 0.1
-# bp = False
-# nm = 'NC'
-
-#setting 3
-# alpha = [-0.1,-0.1,0.2,0.2,.15,.15]
-# alpha_err = 0.1
-# bp = False
-# nm = 'NC'
-
-#setting 4
-# alpha = [-0.1,-0.1,0.2,0.2,.15,.15]
-# alpha_err = 0.1
-# bp = True
-# nm = 'NC'
-
-#setting 5
-alpha = [-0.1,-0.1,0.2,0.2,.15,.15]
-alpha_err = 0.1
-bp = True
-nm = 'TOD'
+alpha = 0
+alpha_err = 0
+bp = False
+nm = 'NC'
 
 # Initialize LATsky and Spectra
-lat = LATsky(libdir, nside, cb_model, beta, alpha=alpha, alpha_err=alpha_err, bandpass=bp,noise_model=nm)
+lat = LATsky(libdir, nside, mask, cb_model, beta, alpha=alpha, alpha_err=alpha_err, bandpass=bp,noise_model=nm, nhits=nhits, nhits_fac=nhits_fac)
 spec = Spectra(lat, libdir, cache=True, parallel=0)
 
 start_i = 0
-end_i = 100
+end_i = 1
 jobs = np.arange(start_i, end_i)
 
 if args.sim:
@@ -87,11 +66,11 @@ if args.specsync:
     mpi.barrier()
 
 if args.mle:
-    fit = "Ad + beta + alpha"
-    binwidth = 10
-    bmin = 50
-    bmax = 2000
-    mle = MLE(libdir,spec,fit, alpha_per_split=False,rm_same_tube=True,binwidth=binwidth,bmin=bmin,bmax=bmax)
+    fit = "Ad + As + beta + alpha"
+    binwidth = 20
+    bmin = 100
+    bmax = 3500
+    mle = MLE(libdir,spec,fit, alpha_per_split=True,rm_same_tube=True,binwidth=binwidth,bmin=bmin,bmax=bmax)
     for i in jobs[mpi.rank::mpi.size]:
         mle.estimate_angles(i)
     mpi.barrier()
